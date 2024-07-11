0c050ad4bab3eeeddddbb89f9d4209a9a2824cca
pablopuch <pablopuch21@hotmail.com>
2024-07-11 13:12:38 +0100

    new corrections for SEO

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,71 +1,20 @@
 export const ui = {
   en: {
-    "nav.twitter": "Twitter",
     "home.welcome": "Welcome to my website",
-    "about.title": "About me",
-    "about.description": "I'm a web developer and I love Astro!",
-    "about.text": `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-		labore et dolore magna aliqua. Vitae ultricies leo integer malesuada nunc vel risus commodo
-		viverra. Adipiscing enim eu turpis egestas pretium. Euismod elementum nisi quis eleifend quam
-		adipiscing. In hac habitasse platea dictumst vestibulum. Sagittis purus sit amet volutpat. Netus
-		et malesuada fames ac turpis egestas. Eget magna fermentum iaculis eu non diam phasellus
-		vestibulum lorem. Varius sit amet mattis vulputate enim. Habitasse platea dictumst quisque
-		sagittis. Integer quis auctor elit sed vulputate mi. Dictumst quisque sagittis purus sit amet.`,
-    "home.p1": `Welcome to the official blog starter template. This template serves as a
-    lightweight, minimally-styled starting point for anyone looking to build a
-    personal website, blog, or portfolio with Astro.`,
-    "home.p2": `This template comes with a few integrations already configured in your
-    <code>astro.config.mjs</code> file. You can customize your setup with
-    <a href="https://astro.build/integrations">Astro Integrations</a> to add
-    tools like Tailwind, React, or Vue to your project.`,
-    "home.p3": "Here are a few ideas on how to get started with the template:",
-    "home.p3.0": "Handle translations in",
-    "home.p3.1": "Edit this page in",
-    "home.p3.2": "Edit translated pages in",
-    "home.p3.3": "Edit the site header items in",
-    "home.p3.4": "Add your name to the footer in",
-    "home.p3.5":
-      "Check out the included blog posts in languages ES, EN and FR in",
-    "home.p3.6": "Customize the general site layout in",
-    "home.p3.7": "Customize the blog post page layout in",
     "site.title": "Libella",
     "sub-header": "Life and Wellness Centre",
     "site.description": "Libella centro de vida y bienestar",
     "nav.home": "Home",
-    "nav.about": "Libella",
     "nav.class": "Class",
     "nav.contact": "Contact",
 
   },
   es: {
     "home.welcome": "Bienvenido a mi sitio web",
-    "about.title": "Sobre mi",
-    "about.description": "Soy un desarrollador web y me encanta Astro!",
-    "about.text": `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-		labore et dolore magna aliqua. Vitae ultricies leo integer malesuada nunc vel risus commodo
-		viverra. Adipiscing enim eu turpis egestas pretium. Euismod elementum nisi quis eleifend quam
-		adipiscing. In hac habitasse platea dictumst vestibulum. Sagittis purus sit amet volutpat. Netus
-		et malesuada fames ac turpis egestas. Eget magna fermentum iaculis eu non diam phasellus
-		vestibulum lorem. Varius sit amet mattis vulputate enim. Habitasse platea dictumst quisque
-		sagittis. Integer quis auctor elit sed vulputate mi. Dictumst quisque sagittis purus sit amet.`,
-    "home.p1": `Bienvenido a la plantilla de inicio de blog oficial. Esta plantilla sirve como punto de partida ligero y con estilo mínimo para cualquiera que quiera construir un sitio web personal, un blog o un portafolio con Astro.`,
-    "home.p2": `Esta plantilla viene con algunas integraciones ya configuradas en su archivo <code>astro.config.mjs</code>. Puede personalizar su configuración con <a href="https://astro.build/integrations">Astro Integrations</a> para agregar herramientas como Tailwind, React o Vue a su proyecto.`,
-    "home.p3": "Aquí hay algunas ideas sobre cómo comenzar con la plantilla:",
-    "home.p3.0": "Maneje las traducciones en",
-    "home.p3.1": "Edite esta página en",
-    "home.p3.2": "Editar páginas traducidas en",
-    "home.p3.3": "Edite los elementos de encabezado del sitio en",
-    "home.p3.4": "Agregue su nombre al pie de página en",
-    "home.p3.5":
-      "Consulte las publicaciones de blog incluidas en idiomas ES, EN y FR en",
-    "home.p3.6": "Personalice el diseño general del sitio en",
-    "home.p3.7":
-      "Personalice el diseño de la página de publicación del blog en",
     "site.title": "Libella",
     "sub-header": "Centro de vida y bienestar",
     "site.description": "Libella centro de vida y bienestar",
     "nav.home": "Inicio",
-    "nav.about": "Libella",
     "nav.class": "Clases",
     "nav.contact": "Contacto",
   },
--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,26 +1,36 @@
 import { ui } from "./ui";
 
+// Objeto que define los idiomas disponibles
 export const LANGUAGES = {
   en: "English",
   es: "Español",
 };
 
+// Idioma por defecto
 export const DEFAULT_LANG = "es";
 
+// Tipo que representa las claves del objeto 'ui'
 export type UiType = keyof typeof ui;
 
+// Función para obtener el idioma desde la URL
 export function getLangFromUrl(url: URL) {
+  // Divide la URL por "/"
   const [, lang] = url.pathname.split("/");
+  // Si el idioma existe en el objeto 'ui', retorna ese idioma
   if (lang in ui) return lang as UiType;
+  // Si no, retorna el idioma por defecto
   return DEFAULT_LANG;
 }
 
+// Función que retorna otra función para traducir las claves de 'ui'
 export function useTranslations(lang?: UiType) {
   return function t(
     key: keyof (typeof ui)[typeof DEFAULT_LANG],
     ...args: any[]
   ) {
+    // Obtiene la traducción del idioma especificado o del idioma por defecto
     let translation = ui[lang ?? DEFAULT_LANG][key] || ui[DEFAULT_LANG][key];
+    // Si hay argumentos, reemplaza los marcadores en la traducción con esos argumentos
     if (args.length > 0) {
       for (let i = 0; i < args.length; i++) {
         translation = translation.replace(`{${i}}`, args[i]);
@@ -30,16 +40,20 @@ export function useTranslations(lang?: UiType) {
   };
 }
 
+// Función para verificar si la ruta está en el idioma especificado
 export function pathNameIsInLanguage(pathname: string, lang: UiType) {
+  // Retorna true si la ruta comienza con el idioma especificado o si es el idioma por defecto y la ruta no comienza con ningún idioma
   return pathname.startsWith(`/${lang}`) || (lang === DEFAULT_LANG && !pathNameStartsWithLanguage(pathname));
 }
 
+// Función auxiliar para verificar si la ruta comienza con un idioma
 function pathNameStartsWithLanguage(pathname: string) {
   let startsWithLanguage = false;
   const languages = Object.keys(LANGUAGES);
 
   for (let i = 0; i < languages.length; i++) {
     const lang = languages[i];
+    // Si la ruta comienza con algún idioma, retorna true
     if (pathname.startsWith(`/${lang}`)) {
       startsWithLanguage = true;
       break;
@@ -49,11 +63,14 @@ function pathNameStartsWithLanguage(pathname: string) {
   return startsWithLanguage;
 }
 
+// Función para obtener la ruta localizaba según el idioma
 export function getLocalizedPathname(pathname: string, lang: UiType) {
   if (pathNameStartsWithLanguage(pathname)) {
+    // Si la ruta comienza con un idioma, reemplaza el idioma en la ruta
     const availableLanguages = Object.keys(LANGUAGES).join('|');
     const regex = new RegExp(`^\/(${availableLanguages})`);
     return pathname.replace(regex, `/${lang}`);
   }
+  // Si no, añade el idioma al comienzo de la ruta
   return `/${lang}${pathname}`;
 }
